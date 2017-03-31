70eac348cf15c6dc94b59d141bf0323b1841229a
Sundelit <sundelit@mail.gvsu.edu>
2017-03-31 09:32:02 -0400

    Edit Class and Constructor

--- CIS163Q.java
+++ CIS163Q.java
@@ -1,4 +1,14 @@
-public class CIS163Q {
+public class CIS163Q<T> {
+
+	private Node<T> first;
+	private Node<T> last;
+	private int size;
+	
+	Public CIS163Q(int s){
+		
+		this.size = s;
+
+	}
 
 
 }
