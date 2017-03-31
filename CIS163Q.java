7035ac71cc73c1fadd7fdd69c7e5654b274d97df
tvanfossen <tristan.vanfossen@yahoo.com>
2017-03-31 09:43:34 -0400

    Adds size function

--- CIS163Q.java
+++ CIS163Q.java
@@ -10,5 +10,19 @@ public class CIS163Q<T> {
 
 	}
 
+	public int size(Node<t> first)
+	{
+		int temp = 0;
+		Node<t> cur = first;
+
+		while (cur.getNextNode().getThisElement() != null)
+		{
+			temp++;
+			cur = cur.getNextNode();
+		}
+
+		return temp;
+	}
+
 
 }
